f76a1b2b5e3e53ffc624b0fffabe6caf0bc23f98
Asher <grit.developer.asher@gmail.com>
2021-10-19 10:12:13 +0900

    수정- 로그인 시 스타일 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
+import { history } from '../redux/configureStore';
 import Button from '../elements/Button';
 import FlexBox from '../elements/FlexBox';
 
 const Header = props => {
+  const is_login = true;
+
+  const moveToMain = () => {
+    history.push('/');
+  };
+  const moveToUser = () => {
+    //유저페이지로 이동
+  };
+
   return (
     <HeaderWrap>
-      <FlexBox //
-        bg_color='#ccc'
-        justify='space-between'
-      >
+      <FlexBox justify='space-between'>
         <Logo>
           <Button //
-            bd='none'
             ft_size='1.8rem'
+            onClick={moveToMain}
           >
             velog
           </Button>
         </Logo>
-        <Button
-          bd_radius='30px'
-          bg_color='#343a3f'
-          color='#ffffff'
-          ft_size='17px'
-        >
-          로그인
-        </Button>
+        {is_login ? (
+          <LoginStBox onClick={moveToUser}>
+            <Button
+              width='50px'
+              height='50px'
+              bd_radius='50%'
+              bg_color='#0097a7'
+              color='#ffffff'
+              ft_size='1.3rem'
+            >
+              S
+            </Button>
+            <ArrowDropDownIcon sx={{ color: '#777' }} />
+          </LoginStBox>
+        ) : (
+          <Button
+            bd_radius='30px'
+            bg_color='#343a3f'
+            color='#ffffff'
+            ft_size='1.1rem'
+          >
+            로그인
+          </Button>
+        )}
       </FlexBox>
     </HeaderWrap>
   );
@@ -36,5 +60,11 @@ const HeaderWrap = styled.header`
   margin-bottom: 60px;
 `;
 const Logo = styled.h1``;
+
+const LoginStBox = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
 
 export default Header;
