26d78610ebd423c803c3415d9f5c404d691e7856
Asher <grit.developer.asher@gmail.com>
2021-10-19 10:15:50 +0900

    수정- router로 컴포넌트들 묶기

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,36 @@
 import './App.css';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route } from 'react-router';
+import styled from 'styled-components';
+
+import PostList from '../pages/PostList';
+import PostWrite from '../pages/PostWrite';
+import PostDetail from '../pages/PostDetail';
+import PostEdit from '../pages/PostEdit';
+import { history } from '../redux/configureStore';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function App() {
-  return <>Hello~</>;
+  return (
+    <>
+      <Container>
+        <Header />
+        <ConnectedRouter history={history}>
+          <Route path={['/', '/post']} exact component={PostList} />
+          <Route path='/post/detail/:id' exact component={PostDetail} />
+          <Route path='/post/write' exact component={PostWrite} />
+          <Route path='/post/edit/:id' exact component={PostEdit} />
+        </ConnectedRouter>
+      </Container>
+      <Footer />
+    </>
+  );
 }
+
+const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
 
 export default App;
