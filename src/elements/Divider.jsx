150ef77a23a06061b5001d11b8f4d8774995c300
Asher <grit.developer.asher@gmail.com>
2021-10-19 10:13:10 +0900

    추가- 최소단위 컴포넌트: 이미지, 구분자

---
+++ src/elements/Divider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Divider = ({ children }) => {
+  return <Item>{children}</Item>;
+};
+
+const Item = styled.span`
+  margin: ${props => (props.margin ? `props.margin` : `0 10px`)};
+`;
+
+export default Divider;
